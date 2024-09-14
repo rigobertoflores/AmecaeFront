c2f1f985b47516dcb88e889fa308198d3e5a2541
rigobertoflores <93140192+rigobertoflores@users.noreply.github.com>
2024-09-14 16:12:08 -0500

    actualizacion de url amecae

--- src/app/Services/Service.ts
+++ src/app/Services/Service.ts
@@ -8,13 +8,13 @@ import { FotoPaciente } from '../interface/FotoPaciente';
   providedIn: 'root',
 })
 export class Service {
-  private api = 'https://localhost:7210/CliniaOv/CliniaOvController/';
-  private apiTratamientos =
-    'https://localhost:7210/api/Tratamientos/TratamientosController/';
-  // private api =
-  //   'https://clinicaolivera.azurewebsites.net/CliniaOv/CliniaOvController/';
+  // private api = 'https://localhost:7210/CliniaOv/CliniaOvController/';
   // private apiTratamientos =
-  //   'https://clinicaolivera.azurewebsites.net/api/Tratamientos/TratamientosController/';
+  //   'https://localhost:7210/api/Tratamientos/TratamientosController/';
+  private api =
+    'https://apiamecae-h7fverbshphnbycs.eastus2-01.azurewebsites.net/CliniaOv/CliniaOvController/';
+  private apiTratamientos =
+    'https://apiamecae-h7fverbshphnbycs.eastus2-01.azurewebsites.net/api/Tratamientos/TratamientosController/';
 
   constructor(private http: HttpClient) {}
 
